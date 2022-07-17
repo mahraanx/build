25ebdc6bf1d0b2c5ec7873d501470bb2ceb89dac
mahraanx <85754388+mahraanx@users.noreply.github.com>
2022-07-17 10:28:20 +0200

    Update functions1.js

--- functions1.js
+++ functions1.js
@@ -3,8 +3,24 @@ const Pass = document.getElementById('pw');
 const LoginBtn = document.getElementById('login');
 
 const database = firebase.database();
-console.log(window.location.origin.toString());
-// var path = window.location.origin.toString().replace('www.build.','');
+$(document).ready(function() {
+path_c = window.location;
+switch (path_c){
+    case "http://192.168.1.37/ryba_testground/":
+    $('#menu-item-22').addClass('current_page_item');
+    alert(path_c);
+    break;
+    case "http://192.168.1.37/ryba_testground/?page_id=7":
+    $('#menu-item-21').addClass('current_page_item');
+    break; 
+    case "http://192.168.1.37/ryba_testground/?page_id=9":
+    $('#menu-item-20').addClass('current_page_item');
+    break;
+    case "http://192.168.1.37/ryba_testground/?page_id=11":
+    $('#menu-item-19').addClass('current_page_item');
+    break;
+}
+});// var path = window.location.origin.toString().replace('www.build.','');
 // var end_of_unique = window.location.origin.toString().indexOf("netlify.app");
 // var path = window.location.origin.toString().substring(0,end_of_unique-1);
 // path = path.replace("https://www.","");
